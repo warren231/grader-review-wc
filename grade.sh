c56d6be9dd068fc5b54ac0310ee88959862bd1d7
Trinh <cs15lwi23acl@ieng6-201.ucsd.edu>
2023-03-01 14:08:41 -0800

    hi

--- grade.sh
+++ grade.sh
@@ -1,10 +1,43 @@
+failure() {
+    echo "YOUR CODE IS A FAILURE!!! $1" >&2
+    exit 1
+}
+
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+cd student-submission
+if [[ ! -f ListExamples.java  ]]
+then
+    failure "ListExamples.java not found in current directory"
+fi
 
+cd ..
 cp student-submission/ListExamples.java ./
-javac -cp $CPATH *.java
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
 
+javac -cp $CPATH *.java
+if [[ $? -ne 0 ]]
+then
+    failure "Failed to compile!!"
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > java.txt
+cat java.txt
+
+LAST=`tail -1 java.txt`
+
+PASS=`echo $LAST | grep -oP '(?<=OK \()\d+'`
+RAN=`echo $LAST | grep -oP '(?<=run: )\d+'`
+FAIL=`echo $LAST | grep -oP '(?<=Failures: )\d+'`
+
+echo hi $RAN $FAIL
+if [[ -n $PASS ]]
+then
+    PASS=`echo "$RAN - $FAIL"`
+    TOTAL=$RAN
+else
+    TOTAL=$PASS
+fi
+echo You passed $PASS / $TOTAL tests
